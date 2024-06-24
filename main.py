235266c4e4f4c2c8b137ab32f2bdb765f00bee0d
Sergey Grigorchuk <grigorchuk@gmail.com>
2024-06-24 15:32:10 +0300

    simplest UI with search

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
-from typing import Union
-from fastapi import FastAPI
-from sqlmodel import SQLModel, Session, create_engine, funcfilter
-import json
+from typing import Annotated
+
+from fastapi import FastAPI, Request, Form
+from sqlmodel import Session, create_engine, select
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 from models import Plugin
 
 db_url = 'postgresql://postgres:mysecretpassword@localhost/postgres'
 engine = create_engine(db_url, echo=True)
 
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    load_data()
-    yield
+app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
 
 
 def get_or_create(session, model, **kwargs):
@@ -25,4 +27,34 @@ def get_or_create(session, model, **kwargs):
         return instance
 
 
-app = FastAPI(lifespan=lifespan)
+@app.get("/plugins")
+def get_plugins_json():
+    with Session(engine) as session:
+        plugins = session.query(Plugin).limit(50).all()
+        return plugins
+
+
+@app.get("/", response_class=HTMLResponse)
+def get_plugins(request: Request):
+    with Session(engine) as session:
+        plugins = session.query(Plugin).limit(50).all()
+        return templates.TemplateResponse(
+            request=request, name="index.html", context={"plugins": plugins}
+        )
+
+
+@app.post("/search", response_class=HTMLResponse)
+def search_plugin(request: Request, search: str = Form(default="")):
+    with Session(engine) as session:
+        if len(search) == 0:
+            plugins = session.query(Plugin).limit(50).all()
+        else:
+            # search within description and title
+            plugins = session.query(Plugin).filter(
+                Plugin.title.ilike(f"%{search}%") | Plugin.description.ilike(f"%{search}%")
+            ).all()
+
+        return templates.TemplateResponse(
+            request=request, name="plugins.html", context={"plugins": plugins}
+        )
+
